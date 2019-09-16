90d31509600557cf5c4c0f8aaf219d4f61e1aade
pjieder <55383141+pjieder@users.noreply.github.com>
2019-09-16 10:59:50 +0200

    Initial commit

---
+++ Ping1/PingWorld.java
@@ -0,0 +1,44 @@
+import greenfoot.*;
+
+
+/**
+ * The Ping World is where Balls and Paddles meet to play pong.
+ * 
+ * @author The teachers 
+ * @version 1
+ */
+public class PingWorld extends World
+{
+    private static final int WORLD_WIDTH = 500;
+    private static final int WORLD_HEIGHT = 700;
+
+    public PingWorld()
+    { 
+        // calling the other constructor with gameStarted = false.
+        this(false);   
+    }
+    
+    
+    /**
+     * Constructor for objects of class PingWorld.
+     */
+    public PingWorld(boolean gameStarted)
+    {
+        super(WORLD_WIDTH, WORLD_HEIGHT, 1); 
+        if (gameStarted)
+        {
+            GreenfootImage background = getBackground();
+            background.setColor(Color.BLACK);
+            // Create a new world with WORLD_WIDTHxWORLD_HEIGHT cells with a cell size of 1x1 pixels.
+            addObject(new Ball(), WORLD_WIDTH/2, WORLD_HEIGHT/2);
+            Paddle player = new Paddle(100,10);
+            addObject(player, 300, 650);
+            addObject(new Computer(100,10),100, WORLD_HEIGHT - 650);
+        }
+        else
+        {
+            Greenfoot.setWorld(new IntroWorld());
+        }
+    }
+
+}
